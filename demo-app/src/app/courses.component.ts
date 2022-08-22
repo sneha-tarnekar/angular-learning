2498c7e099d15e3db3a47f0c65e1687a40816e8f
sneha <sneha.tarnekar@thinkitive.com>
2022-08-22 19:53:05 +0530

    Implement Property binding, Attribute binding Add bootstrap for Angular application Implement class, style binding (variation of property binding) Perform Event binding and Event filtering

--- demo-app/src/app/courses.component.ts
+++ demo-app/src/app/courses.component.ts
@@ -4,21 +4,32 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses', 
     template: `
-                <h2>{{ getTitle() }}</h2>
+                <h2>{{ title }}</h2>
+                <h2 [textContent]="title" [style.backgroundColor]="isActive ? 'blue' : 'gray'"></h2>
                 <ul>
                     <li *ngFor="let course of courses">
                         {{ course }} 
                     </li>
                 </ul>
+                <table>
+                    <tr>
+                      <td [attr.colspan]="colSpan"></td>
+                    </tr>
+                </table>
+
+                <input #email (keyup.enter)="onKeyUp(email.value)" />
+
+                <div (click)="onDivClicked()">
+                    <button class="btn btn-primary" [class.active]="isActive" (click)="onSave()">Save</button>
+                </div>
     `
 })
 export class CoursesComponent {
     title = "List of courses";
+    colSpan = 2;
+    isActive = false;
     courses;
-
-    getTitle() {
-        return this.title;
-    }
+    email: any;
 
     // Dependency Injection
     // This contructor has dependency on CoursesService
@@ -26,6 +37,18 @@ export class CoursesComponent {
     constructor(service: CoursesService) {
         // let service = new CoursesService();
         this.courses = service.getCourses();
+    }
+
+    onSave() {
+        console.log("Button is clicked")
+    }
+
+    onDivClicked() {
+        console.log("Div is clicked")
+    }
+
+    onKeyUp(email: any) {
+        console.log(email);
     }
 
 }
